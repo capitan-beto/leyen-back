cc02c6480a4931726a31b9573c860652ac6c685f
Otroletravaladna <crnana98@gmail.com>
2024-11-06 23:25:11 -0300

    create api responses module

---
+++ api/api.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type HealthResponse struct {
+	Code    int
+	Message string
+}
+
+//error handling
+
+type Error struct {
+	Code    int
+	Message string
+}
+
+func writeError(w http.ResponseWriter, msg string, code int) {
+	res := Error{
+		Code:    code,
+		Message: msg,
+	}
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	json.NewEncoder(w).Encode(res)
+}
+
+var (
+	RequestErrorHandler = func(w http.ResponseWriter, err error) {
+		writeError(w, err.Error(), http.StatusBadRequest)
+	}
+	InternalErrorHandler = func(w http.ResponseWriter) {
+		writeError(w, "An unexpected Error Ocurred", http.StatusInternalServerError)
+	}
+	UnauthorizedHandler = func(w http.ResponseWriter, err error) {
+		writeError(w, err.Error(), http.StatusUnauthorized)
+	}
+	NotFoundHandler = func(w http.ResponseWriter, err error) {
+		writeError(w, err.Error(), http.StatusNotFound)
+	}
+)
