bd20d0b3cfee9f51b291d48d5ef19246438c0330
Otroletravaladna <crnana98@gmail.com>
2024-11-06 22:21:41 -0300

    feat: mount router, tcp listener, set port and call Handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
-	fmt.Println("Working babiee!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Error(err)
+	}
+
+	PORT, exists := os.LookupEnv("PORT")
+	if exists == false {
+		os.Setenv("PORT", "8080")
+	}
+
+	log.SetReportCaller(true)
+	var r *chi.Mux = chi.NewRouter()
+	handlers.Handler(r)
+
+	fmt.Println("Starting GO API service...")
+
+	fmt.Println(`
+	______     ______        ______     ______   __    
+   /\  ___\   /\  __ \      /\  __ \   /\  == \ /\ \   
+   \ \ \__ \  \ \ \/\ \     \ \  __ \  \ \  _-/ \ \ \  
+	\ \_____\  \ \_____\     \ \_\ \_\  \ \_\    \ \_\ 
+	 \/_____/   \/_____/      \/_/\/_/   \/_/     \/_/ `)
+
+	fmt.Println(("Running on " + PORT + "!"))
+	log.Fatal(http.ListenAndServe(":"+PORT, r))
 }
