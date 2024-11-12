6b6e77e23c9eab4680799c608f2c93ef48d05c6a
Otroletravaladna <crnana98@gmail.com>
2024-11-12 01:10:59 -0300

    Create password hashing module

---
+++ utils/hash_pass.go
@@ -0,0 +1,19 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(pass string) (string, error) {
+	var passBytes = []byte(pass)
+
+	hashedPassBytes, err := bcrypt.
+		GenerateFromPassword(passBytes, bcrypt.MinCost)
+
+	return string(hashedPassBytes), err
+}
+
+func CheckPassword(hashedPassword, clientPass string) bool {
+	err := bcrypt.CompareHashAndPassword(
+		[]byte(hashedPassword), []byte(clientPass))
+
+	return err == nil
+}
