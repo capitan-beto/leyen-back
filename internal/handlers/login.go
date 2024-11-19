a6681813d6d966aada71e16f2ac0dc368b8605b1
Otroletravaladna <crnana98@gmail.com>
2024-11-19 19:07:44 -0300

    create login service

---
+++ internal/handlers/login.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"cmd/api/main.go/api"
+	"cmd/api/main.go/internal/tools"
+	"cmd/api/main.go/models"
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	var user *models.User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	var database *sql.DB
+	database, err := tools.CreateConnection()
+	if err != nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return
+	}
+
+	if role, ok := tools.CheckUserData(user.Email, user.Pass, database); ok {
+		tokenString, err := utils.GenerateToken(user.Email, *&role)
+		if err != nil {
+			log.Error(err)
+			api.RequestErrorHandler(w, err)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		if err = json.NewEncoder(w).Encode(tokenString); err != nil {
+			log.Error(err)
+			api.InternalErrorHandler(w)
+		}
+		return
+	} else {
+		api.UnauthorizedHandler(w, errors.New("invalid credentials"))
+		return
+	}
+}
