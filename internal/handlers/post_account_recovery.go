1c6f8a65478350071dfcadb8e8af5bf563263ba2
Otroletravaladna <crnana98@gmail.com>
2024-11-25 21:33:45 -0300

    refactor account recoery module

--- internal/handlers/post_account_recovery.go
+++ internal/handlers/post_account_recovery.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -11,10 +12,12 @@ import (
 	"cmd/api/main.go/models"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/xlzd/gotp"
 )
 
-func PostSendEmail(w http.ResponseWriter, r *http.Request) {
-	var reqBody *models.EmailReqBody
+func PostAccountRecovery(w http.ResponseWriter, r *http.Request) {
+	var reqBody *models.OTTPRequest
+
 	err := json.NewDecoder(r.Body).Decode(&reqBody)
 	if err != nil {
 		log.Error(err)
@@ -22,9 +25,26 @@ func PostSendEmail(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	defer r.Body.Close()
+
+	totp := gotp.NewDefaultTOTP(gotp.RandomSecret(16))
+
+	var database *sql.DB
+	database, err = tools.CreateConnection()
+	if err != nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+	}
+
+	err = tools.UpdateLastTOTP(reqBody.ToAddr, totp.Now(), database)
+	if err != nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+	}
+
 	to := strings.Split(reqBody.ToAddr, ",")
 
-	err = tools.SendEmail(to, reqBody.Subj, reqBody.Body)
+	err = tools.SendEmail(to, "Login con código único", fmt.Sprintf("<a>%s</a>", totp.Now()))
 	if err != nil {
 		log.Error(err)
 		api.InternalErrorHandler(w)
@@ -33,7 +53,7 @@ func PostSendEmail(w http.ResponseWriter, r *http.Request) {
 
 	var res = api.PostHandlerResponse{
 		Code:    http.StatusOK,
-		Message: fmt.Sprintf("email sent to: %s", to),
+		Message: fmt.Sprintf("one tiime code sent to %s", to),
 	}
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -44,5 +64,4 @@ func PostSendEmail(w http.ResponseWriter, r *http.Request) {
 		api.InternalErrorHandler(w)
 		return
 	}
-
 }
